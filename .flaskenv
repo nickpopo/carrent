fbebc5558246c5e72df1f1b4301bfc0efb360241
Nikolay Popov <koyabica@gmail.com>
2019-12-26 21:03:56 +0300

    heroku deployment changes

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,1 @@
 FLASK_APP=carrent.py
-FLASK_DEBUG=1
--- config.py
+++ config.py
@@ -15,11 +15,11 @@ class Config(object):
 
 	# Mail settings
 	MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'localhost'
-	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 8025)
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
 	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	ADMINS = ['admin@ex.com']
+	ADMINS = ['koyabica@gmail.com']
 
 	# Frontside settings.
-	POSTS_PER_PAGE = 3
+	POSTS_PER_PAGE = 10
